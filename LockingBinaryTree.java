2004f1aa3d2236233771bfb2341e9f6aea25e9d8
praveens96 <praveen13400@gmail.com>
2021-04-29 21:54:38 +0530

    updated program for all cases

--- LockingBinaryTree.java
+++ LockingBinaryTree.java
@@ -19,104 +19,6 @@ public class Main
     
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-	}
-}
-
-class LockingBinaryTree {
-    
-    private class Node {
-        int data;
-        Node left;
-        Node right;
-        Node parent;
-        boolean locked;
-        int descendantLockCount;
-    }
-    Node root;
-    boolean isLocked(Node n){
-        return n.locked;
-    }
-    boolean lock(Node n) {
-        // if all descendants and ancestors are not locked only we can lock
-        if(n.locked == true){
-            return true;
-        }
-        if(n.descendantLockCount > 0){
-            return false;
-        }
-        if(n.parent == null){
-            // root node
-            return true;
-        } else {
-            if(!canLockOrUnlock(n)) {
-                return false;
-            } else {
-                n.locked = true;
-                //increment
-                incrementLockCount(n.parent);
-                return true;
-            }
-        }
-        
-    }
-    boolean canLockOrUnlock(Node n) {
-        Node current = n.parent;
-            while(current.parent != null){
-                if(current.descendantLockCount > 0){
-                    // break;
-                    return false;
-                }
-                current = current.parent;
-            }
-            return true;
-    }
-    void incrementLockCount(Node p){
-        while(p.parent != null){
-            p.descendantLockCount += 1;
-            p = p.parent;
-        }
-    }
-    void decrementLockCount(Node p){
-        while(p.parent != null){
-            p.descendantLockCount -= 1;
-            p = p.parent;
-        }
-    }
-    boolean unlock(Node n) {
-        if(!canLockOrUnlock(n)) {
-            return false;
-        } else {
-        n.locked = false;
-        //decrement count
-        decrementLockCount(n);
-        return true;
-        }
-    }
-    
-}
-
-/**
-
-
-/******************************************************************************
-This problem was asked by Google.
-
-Implement locking in a binary tree. A binary tree node can be locked or unlocked only if all of its descendants or ancestors are not locked.
-
-Design a binary tree node class with the following methods:
-
-is_locked, which returns whether the node is locked
-lock, which attempts to lock the node. If it cannot be locked, then it should return false. Otherwise, it should lock it and return true.
-unlock, which unlocks the node. If it cannot be unlocked, then it should return false. Otherwise, it should unlock it and return true.
-You may augment the node to add parent pointers or any other property you would like. You may assume the class is used in a single-threaded program, so there is no need for actual locks or mutexes. Each method should run in O(h), where h is the height of the tree.
-
-*******************************************************************************/
-
-public class Main
-{
-    
-	public static void main(String[] args) {
-		System.out.println("Hello World");
 		Node lbt = new Node(1);
 		lbt.parent = null;
 		lbt.left = new Node(2);
@@ -125,24 +27,17 @@ public class Main
 		lbt.right.parent = lbt;
 		lbt.left.left = new Node(4);
 		lbt.left.right = new Node(5);
-		lbt.left.parent = lbt.left;
+		lbt.left.left.parent = lbt.left;
 		lbt.right.left = new Node(6);
 		lbt.right.right = new Node(7);
-		lbt.right.parent = lbt.right;
+		lbt.right.right.parent = lbt.right;
 		boolean haslocked1 = lbt.left.lock();
 		System.out.println(haslocked1);
-// 		Node 
+		boolean haslocked2 = lbt.left.unlock();
+		System.out.println(haslocked2);
+		System.out.println(lbt.descendantLockCount);
 	}
 }
-
-// class LockingBinaryTree {
-//     Node root;
-//     public LockingBinaryTree(int val, boolean locked) {
-//         Node newNode = new Node(val, locked);
-//         newNode.parent = null;
-//         newNode.left = null;
-//         newNode.right = null;
-//     }
     class Node {
         int data;
         Node left;
@@ -157,34 +52,34 @@ public class Main
         }
     
     
-    boolean isLocked(Node n){
-        return n.locked;
+    boolean isLocked(){
+        return this.locked;
     }
-    boolean lock(Node n) {
+    boolean lock() {
         // if all descendants and ancestors are not locked only we can lock
-        if(n.locked == true){
+        if(this.locked == true){
             return true;
         }
-        if(n.descendantLockCount > 0){
+        if(this.descendantLockCount > 0){
             return false;
         }
-        if(n.parent == null){
+        if(this.parent == null){
             // root node
             return true;
         } else {
-            if(!canLockOrUnlock(n)) {
+            if(!canLockOrUnlock(this)) {
                 return false;
             } else {
-                n.locked = true;
+                this.locked = true;
                 //increment
-                incrementLockCount(n.parent);
+                incrementLockCount(this);
                 return true;
             }
         }
         
     }
-    boolean canLockOrUnlock(Node n) {
-        Node current = n.parent;
+    private boolean canLockOrUnlock(Node n) {
+        Node current = n;//.parent;
             while(current.parent != null){
                 if(current.descendantLockCount > 0){
                     // break;
@@ -194,28 +89,27 @@ public class Main
             }
             return true;
     }
-    void incrementLockCount(Node p){
+    private void incrementLockCount(Node p){
         while(p.parent != null){
-            p.descendantLockCount += 1;
+            p.parent.descendantLockCount += 1;
             p = p.parent;
         }
     }
-    void decrementLockCount(Node p){
+    private void decrementLockCount(Node p){
         while(p.parent != null){
-            p.descendantLockCount -= 1;
+            p.parent.descendantLockCount -= 1;
             p = p.parent;
         }
     }
-    boolean unlock(Node n) {
-        if(!canLockOrUnlock(n)) {
+    boolean unlock() {
+        if(!canLockOrUnlock(this)) {
             return false;
         } else {
-        n.locked = false;
+        this.locked = false;
         //decrement count
-        decrementLockCount(n);
+        decrementLockCount(this);
         return true;
         }
     }
     
 }
-*/
